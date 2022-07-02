5d8f79bb20ad80a6d29b16cdb558edcbfb4303bc
Mferreyra01 <105468081+Mferreyra01@users.noreply.github.com>
2022-07-01 21:34:55 -0300

    Add files via upload

---
+++ proyecto_pp2/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+   
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- Bootstrap -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<link href="bootstrap.min.css" rel="stylesheet">
+
+	<title>MENU</title>
+	<meta charset="utf-8">
+    <!-- Estilos en css -->
+
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+
+</head>
+<body id="body" class="m=0 row justify-content-center text-center">
+
+<nav><h1>Proyecto PP2  | Speed Data</h1></nav>
+
+        <div id="menu">
+                                          <!-- MENU -->
+<br>
+    
+       <div class="btn-group-vertical " >                                   <!--Color borde|tipo de boton|alineacion|tamaño -->
+       <a href="produ.html " button type="button" style="margin: 5px"   class="border-dark rounded btn btn-outline-primary btn-block  btn-lg "><h2>PRODUCCION</h2></button></a>
+        <a href="stock.html " button type="button" style="margin: 5px" class="border-dark rounded btn btn-outline-primary btn-blcok  btn-lg "><h2>STOCK</H2></button></a>
+        <a href="imp_etiq.html " button type="button" style="margin: 5px" class="border-dark rounded btn btn-outline-primary btn-block  btn-lg"><h2>IMP. DE ETIQUETAS</h2></button></a>
+        <a href="imp_etiq.html " button type="button" style="margin: 5px" class="border-dark rounded btn btn-outline-primary btn-block  btn-lg"><h2>RECETAS</h2></button></a>
+    </div>
+</div>
+
+
+   
+
+     <!-- Bootstrap -->  
+    
+     <footer>
+     &copy; 2022
+</footer>
+</body>
+</html>
