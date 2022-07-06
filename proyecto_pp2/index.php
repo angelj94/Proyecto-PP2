1ef2cc2068547995f9c09574cdd91a61094bdfe3
angelj94 <105680614+angelj94@users.noreply.github.com>
2022-07-06 15:25:15 -0300

    Cambios en bootstrap

--- proyecto_pp2/index.php
+++ proyecto_pp2/index.php
@@ -4,10 +4,9 @@
    
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<!-- Bootstrap -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<link href="bootstrap.min.css" rel="stylesheet">
 
+<!-- Bootstrap -->
+<link rel="stylesheet" href="css/bootstrap.min.css"
 	<title>MENU</title>
 	<meta charset="utf-8">
     <!-- Estilos en css -->
@@ -27,7 +26,7 @@
        <a href="produ.html " button type="button" style="margin: 5px"   class="border-dark rounded btn btn-outline-primary btn-block  btn-lg "><h2>PRODUCCION</h2></button></a>
         <a href="stock.html " button type="button" style="margin: 5px" class="border-dark rounded btn btn-outline-primary btn-blcok  btn-lg "><h2>STOCK</H2></button></a>
         <a href="imp_etiq.html " button type="button" style="margin: 5px" class="border-dark rounded btn btn-outline-primary btn-block  btn-lg"><h2>IMP. DE ETIQUETAS</h2></button></a>
-        <a href="imp_etiq.html " button type="button" style="margin: 5px" class="border-dark rounded btn btn-outline-primary btn-block  btn-lg"><h2>RECETAS</h2></button></a>
+        <a href="recetas.html " button type="button" style="margin: 5px" class="border-dark rounded btn btn-outline-primary btn-block  btn-lg"><h2>RECETAS</h2></button></a>
     </div>
 </div>
 
